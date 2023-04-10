205796280571899a68e34961a2171fd208988fa2
Ross Ferguson <ross.c.ferguson@btinternet.com>
2023-04-10 08:38:14 +0100

    Minor tweaks for PowerShell 7

--- BuildAll.ps1
+++ BuildAll.ps1
@@ -2,6 +2,12 @@
 # Build all solutions in directory
 #
 
+if(    $PSVersionTable.PSVersion.Major -ge 7 `
+  -or ($PSVersionTable.PSVersion.Major -eq 7 -and $PSVersionTable.PSVersion.Minor -ge 2))
+{
+    $PSStyle.Progress.View="Classic" # Backwards compatible
+}
+
 Write-Progress -Activity "Gathering solutions..." -Status 0 -PercentComplete 0
     
 $solutions = Get-ChildItem -Path . -Filter *.sln -ErrorAction SilentlyContinue -Force -Recurse -Depth 1
@@ -18,7 +24,7 @@ foreach ($solution in $solutions)
 {
     $complete = [math]::Round(($count / $solutions.Count) * 100)
     
-	Write-Progress -Activity "Building $solution" -Status "$complete%" -PercentComplete $complete
+	Write-Progress -Activity "Building $($solution.BaseName)" -Status "$complete%" -PercentComplete $complete
     
 	$proc = Start-Process -Wait -PassThru -NoNewWindow -FilePath "msbuild.exe" -WorkingDirectory $solution.DirectoryName -ArgumentList $solution.Name, "/verbosity:quiet", "/nologo" #,"/p:Configuration=Debug"
 
